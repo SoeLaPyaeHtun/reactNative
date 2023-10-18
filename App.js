88317813f7904cfcf0273ced00fd7c38c024b9f0
SoeLaPyaeHtun <soelapyaehtun@gmail.com>
2023-10-19 00:25:49 +0800

    updated with github

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This is updated version</Text>
+      <Text>This is updated version with github</Text>
       <StatusBar style="auto" />
     </View>
   );
